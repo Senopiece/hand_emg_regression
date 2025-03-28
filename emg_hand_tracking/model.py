c07f603abe7cf138a3221c4d4232e802b77a95cc
Nabuki <nabuki@vk.com>
2025-03-28 12:27:59 +0300

    fix lr

--- emg_hand_tracking/model.py
+++ emg_hand_tracking/model.py
@@ -152,7 +152,7 @@ class Model(pl.LightningModule):
         self._step("val", batch)
 
     def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=1e-5)
+        return torch.optim.Adam(self.parameters(), lr=1e-4)
 
     def forward(self, x):
         """
