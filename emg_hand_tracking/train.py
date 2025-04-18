a8c01015b18693a0669bec8c5cf2b595a6672ec5
Nabuki <nabuki@vk.com>
2025-04-18 23:20:58 +0300

    less patience

--- emg_hand_tracking/train.py
+++ emg_hand_tracking/train.py
@@ -65,7 +65,7 @@ def run_single(
             ),
             EarlyStopping(
                 monitor="val_loss",
-                patience=30,
+                patience=20,
                 mode="min",
             ),
         ],
